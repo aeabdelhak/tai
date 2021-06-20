2863df38a068086bb34e9644a77a87927843f18f
abdelhak aitelhad <aeaphones@gmail.com>
2021-06-20 03:30:11 +0100

    commit

---
+++ components/NotFound.tsx
@@ -0,0 +1,14 @@
+const NotFound = () => {
+    return (
+        <div className="h-screen  w-screen grid place-items-center">
+          <div>
+          <h1 className="text-7xl text-red-600 font-thin">ops!</h1>
+            <h1 className="text-4xl font-thin ">
+                the page you are requisting is not available 
+            </h1>
+          </div>
+        </div>
+    );
+}
+
+export default NotFound;
